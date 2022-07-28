72a8451ee3e1830ccc213a5bba4b752867ea2e90
Jesse <jesseponce233@gmail.com>
2022-07-28 16:43:25 -0500

    routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,17 +4,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { model: Tag, through: ProductTag, as: 'tagged_products'},
+        { model: Category},
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {model: Tag, through: ProductTag, as: 'tagged_products' },
+        { model: Category},
+      ],
+    });
 
+    if(!productData) {
+      res.status(404).json({ message: 'No product found with this id'});
+      return; 
+    };
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
@@ -89,8 +115,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: 'No product found with this id'});
+      return; 
+    };
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
